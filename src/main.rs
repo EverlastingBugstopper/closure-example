a41cddf2f6752693cb99d1482db71e73558e4139
Avery Harnish <averyharnish@gmail.com>
2019-11-25 15:19:20 -0600

    Add cacher struct definition

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T> {
+    calculation: T,
+    value: Option<u32>,
+}
+
 impl<T> Cacher<T>
 where
     T: Fn(u32) -> u32,
@@ -25,7 +30,7 @@ where
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = Cacher::new(|num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
